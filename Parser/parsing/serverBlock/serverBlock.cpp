3f41ffc77977a7ab24a1a6c4028549c46fec93a8
Mohammed Nasar <mnasar@shi-r2-p4.s19.be>
2024-03-22 11:44:38 +0100

    Parser 22/03

---
+++ Parser/parsing/serverBlock/serverBlock.cpp
@@ -0,0 +1,164 @@
+#include "serverBlock.hpp"
+#include <cstring>
+
+// Constructor
+ServerBlock::ServerBlock() : listen(NULL), root(NULL), index(NULL), uploadFiles(NULL), errorPage(NULL), firstLocation(NULL), next(NULL)
+{
+    serverName = strdup("localhost");
+    this->maxBodySize = 1024;
+}
+
+// Destructor
+
+ServerBlock::~ServerBlock()
+{
+    if (listen != NULL)
+        delete[] listen;
+    if (serverName != NULL)
+        delete[] serverName;
+    if (root != NULL)
+        delete[] root;
+    if (index != NULL)
+        delete[] index;
+    if (uploadFiles != NULL)
+        delete[] uploadFiles;
+    if (errorPage != NULL)
+        delete[] errorPage;
+    if (firstLocation != NULL)
+        delete firstLocation;
+    if (next != NULL)
+        delete next;
+}
+
+// Setters
+void ServerBlock::setListen(const char *newListen)
+{
+    if (listen != NULL)
+        delete[] listen;
+    listen = new char[strlen(newListen) + 1];
+    strcpy(listen, newListen);
+}
+
+void ServerBlock::setServerName(const char *newServerName)
+{
+    if (serverName != NULL)
+        delete[] serverName;
+    serverName = new char[strlen(newServerName) + 1];
+    strcpy(serverName, newServerName);
+}
+
+void ServerBlock::setRoot(const char *newRoot)
+{
+    if (root != NULL)
+        delete[] root;
+    root = new char[strlen(newRoot) + 1];
+    strcpy(root, newRoot);
+}
+
+void ServerBlock::setIndex(const char *newIndex)
+{
+    int newIndexLength = strlen(newIndex);
+    int tempLength;
+    char *temp = index;
+
+    if (temp != NULL)
+    {
+        tempLength = strlen(temp);
+        index = new char[tempLength + newIndexLength + 2];
+        strcpy(index, temp);
+        strcat(index, " ");
+        strcat(index, newIndex);
+        delete[] temp;
+    }
+    else
+    {
+        index = new char[newIndexLength + 1];
+        strcpy(index, newIndex);
+    }
+}
+
+void    ServerBlock::setUploadFiles(const char *newUploadFiles)
+{
+    if (uploadFiles != NULL)
+        delete[] uploadFiles;
+    uploadFiles = new char[strlen(newUploadFiles) + 1];
+    strcpy(uploadFiles, newUploadFiles);
+}
+
+void    ServerBlock::setErrorPage(const char *newErrorPage)
+{
+    int newErrorPageLength = strlen(newErrorPage);
+    int tempLength;
+    char *temp = errorPage;
+
+    if (temp != NULL)
+    {
+        tempLength = strlen(temp);
+        errorPage = new char[tempLength + newErrorPageLength + 2];
+        strcpy(errorPage, temp);
+        strcat(errorPage, " ");
+        strcat(errorPage, newErrorPage);
+        delete[] temp;
+    }
+    else
+    {
+        errorPage = new char[newErrorPageLength + 1];
+        strcpy(errorPage, newErrorPage);
+    }
+}
+
+
+void    ServerBlock::setMaxBodySize(int newBodySize)
+{
+    maxBodySize = newBodySize;
+}
+
+// Getters
+const char *ServerBlock::getListen() const
+{
+    return (listen);
+}
+
+const char *ServerBlock::getServerName() const
+{
+    return (serverName);
+}
+
+const char *ServerBlock::getRoot() const
+{
+    return (root);
+}
+
+const char *ServerBlock::getIndex() const
+{
+    return (index);
+}
+
+const char    *ServerBlock::getUploadFiles(void ) const
+{
+    return (uploadFiles);
+}
+
+const char    *ServerBlock::getErrorPage(void ) const
+{
+    return (errorPage);
+}
+
+int ServerBlock::getMaxBodySize(void ) const
+{
+    return (maxBodySize);
+}
+
+// Linked list
+
+LocationBlock   *ServerBlock::lastLocation(void )
+{
+    LocationBlock   *iterator;
+
+    iterator = firstLocation;
+    if (iterator == NULL)
+        return (NULL);
+    while (iterator->next != NULL)
+        iterator = iterator->next;
+    return (iterator);
+}
---
+++ Parser/parsing/whichLine.cpp
@@ -0,0 +1,100 @@
+#include "header.hpp"
+
+static int whatDirective(char *line)
+{
+    if (strncmp(line, "listen ", 7) == 0)
+        return (7);
+    if (strncmp(line, "server_name ", 12) == 0)
+        return (12);
+    if (strncmp(line, "root ", 5) == 0)
+        return (5);
+    if (strncmp(line, "index ", 6) == 0)
+        return (6);
+    if (strncmp(line, "error_page ", 11) == 0)
+        return (11);
+    if (strncmp(line, "client_max_body_size ", 21) == 0)
+        return (21);
+    if (strncmp(line, "upload_files ", 13) == 0)
+        return (13);
+    return (0);
+}
+
+static int validValue(char *value, int type)
+{
+    char    *valueDup;
+
+    if (value == NULL)
+        return (-1);
+    valueDup = strdup(value);
+    if (valueDup == NULL)
+        return (-1);
+    *(strchr(valueDup, ';')) = '\0';
+    if ((type == 11 && countWords(valueDup) == 2) || (type != 11 && countWords(valueDup) == 1) || ((type == 7 || type == 6) && countWords(valueDup) >= 1))
+    {
+        free(valueDup);
+        return (type);
+    }
+    free(valueDup);
+    return (-1);
+}
+
+int isRegularLine(char *line)
+{
+    int i;
+    char *semiColon;
+
+    if (line == NULL)
+        return (-1);
+    semiColon = strchr(line, ';');
+    if (strchr(line, ' ') == NULL || semiColon == NULL || *(semiColon + 1) != '\0')
+        return (-1);
+    else if ((i = whatDirective(line)) == 0)
+        return (-1);
+    return (validValue(line + i, i));
+}
+
+int isServerLine(char *line)
+{
+    if (line == NULL)
+        return (-1);
+    if (strcmp(line, "server {") == 0 || strcmp(line, "server{") == 0)
+        return (1);
+    return (0);
+}
+
+int isLocationLine(char *line)
+{
+    int i = 0;
+    char    *curlyBrace;
+
+    if (line == NULL)
+        return (-1);
+    trimEnd(line);
+    while (std::isspace(line[i]))
+        i++;
+    curlyBrace = strchr(line, '{');
+    if (strncmp(line, "location ", 9) == 0 && curlyBrace != NULL && *(curlyBrace + 1) == '\0' && countWords(line) == 3)
+        return (1);
+    return (0);
+}
+
+int serverTypeOfLine(char *line)
+{
+    int i;
+
+    if (line == NULL)
+        return (-1);
+    i = 0;
+    while (line[i] != '\0' && std::isspace(line[i]))
+        i++;
+    trimEnd(line);
+    if (line[i] == '\0')
+        return (0);
+    else if (isServerLine(line + i) == 1)
+        return (1);
+    else if (strcmp(line + i, "}") == 0)
+        return (2);
+    else if (isLocationLine(line + i) == 1)
+        return (3);
+    return (isRegularLine(line + i));
+}
